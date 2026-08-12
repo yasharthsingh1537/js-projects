ac6b5008b43b1659efbf7a2b248bf1a0bf4c286e
Yasharth Singh <yasharthsingh.1537@gmai.com>
2026-08-13 00:24:17 +0530

    Made a color flipper game

--- Color Flipper/ColorFlipper.js
+++ Color Flipper/ColorFlipper.js
@@ -1,40 +1,31 @@
-const container = document.querySelector('.container')
-const display = document.querySelector('.display')
-const btn = document.querySelector('button')
+const container = document.querySelector('.container');
+const display = document.querySelector('.display');
+const btn = document.querySelector('button');
 
 // function to generate hex color
 function generateHexColor() {
-    // hex colors
-    const hexChars = "0123456789ABCDEF"
-    let color = "#"
-    for (let i = 0; i < 6; i++) {
-        color += hexChars[Math.floor(Math.random() * 16)]
-    }
-    return color;
+	// hex colors
+	const hexChars = '0123456789ABCDEF';
+	let color = '#';
+	for (let i = 0; i < 6; i++) {
+		color += hexChars[Math.floor(Math.random() * 16)];
+	}
+	return color;
 }
 
 btn.addEventListener('click', () => {
-    const color1 = generateHexColor()
-    const color2 = generateHexColor()
-    container.style.background = `linear-gradient(to right,${color1},${color2})`
-    btn.style.background = `linear-gradient(to right,${color2},${color1})`
-    display.textContent = `${color1} ${color2}`
-})
+	const color1 = generateHexColor();
+	const color2 = generateHexColor();
+	container.style.background = `linear-gradient(to right,${color1},${color2})`;
+	btn.style.background = `linear-gradient(to right,${color2},${color1})`;
+	display.textContent = `${color1} ${color2}`;
+});
 
 display.addEventListener('click', () => {
-    const color = display.textContent
-    navigator.clipboard.writeText(color)
-    display.textContent = 'Copied!'
-    setTimeout(() => { display.textContent = color }, 1000)
-})
-
-
-
-// btn.addEventListener('mouseenter', () => {
-//     setInterval(() => {
-//         const color = generateHexColor()
-//         container.style.backgroundColor = color
-//         btn.style.backgroundColor = color
-//         display.textContent = color
-//     }, 100);
-// })
+	const color = display.textContent;
+	navigator.clipboard.writeText(color);
+	display.textContent = 'Copied!';
+	setTimeout(() => {
+		display.textContent = color;
+	}, 1000);
+});
